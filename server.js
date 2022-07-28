bb6c4ccdf98c49246fa1bf70d074681da6621d1b
Davon <dkwebb95@gmail.com>
2022-07-27 22:57:40 -0400

    called databases to server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -5,6 +6,21 @@ const app = express();
 //Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //MySQL username,
+        user: 'root',
+        //MySQL password
+        password: 'B007CmP2022',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 //Default response for any other request (Not Found)
 app.use((req, res) => {
